f059ef4a79e8dafb4226f47fe28baaf56e743cc0
Winton Davies <wdavies@cs.stanford.edu>
2014-01-30 22:26:38 -0800

    Mapping completed. YMMV. Yet to test upload

--- greatergiving_to_biddingforgood.py
+++ greatergiving_to_biddingforgood.py
@@ -3,6 +3,7 @@
 import sys
 import csv
 import argparse
+import re
 
 parser = argparse.ArgumentParser()
 
@@ -17,11 +18,19 @@ args = parser.parse_args()
 
 b4g_header=['Lot Number', 'Quantity', 'Item Name', 'Item Description', 'Special Note', 'Donor Name', 'Donor URL', 'Donor Logo', 'Category', 'Item Type', 'Item Attributes', 'Item Location', 'Featured Item', 'Bidder Community', 'Cost', 'Estimated Value', 'Value Display', 'Online Bidding', 'Opening Bid', 'Reserve Price', 'Bid Increment', 'Buy Now Price', 'Online Open Date', 'Online Open Time', 'Online Close Date', 'Online Close Time', 'Status', 'Live Event', 'Item Image']
 
+def clean(string):
+    if string is not None:
+        #string = re.sub('[^0-9A-Za-z!,\.:\/]', ' ', string)
+        string = re.sub('[\r\n]', ' ', string)
+        string = re.sub('[ ]{2,}', ' ', string)
+    return string 
+
+
 def convert_gg_to_b4g(gg_dict):
     b4g_dict={}
     b4g_dict['Lot Number'] = gg_dict['Item Number']
     b4g_dict['Item Name'] = gg_dict['Item Name']
-    b4g_dict['Item Description'] = gg_dict['Description']
+    b4g_dict['Item Description'] = clean(gg_dict['Description'])
     b4g_dict['Donor Name'] = gg_dict['Donors']
     b4g_dict['Item Location'] = gg_dict['Location']
     b4g_dict['Cost'] = gg_dict['Cost']
@@ -34,7 +43,7 @@ input_file = csv.DictReader(args.infile)
 for row in input_file:
     gg_rows.append(row)
 
-sys.stderr.write(len(gg_rows) " rows read from input.")
+sys.stderr.write("%d rows read from input.\n" % len(gg_rows))
 
 b4g_rows = map(convert_gg_to_b4g, gg_rows)
 
