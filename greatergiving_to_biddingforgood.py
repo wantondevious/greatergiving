df44094360c4e48892ef83cbd140868840093b63
Winton Davies <wdavies@cs.stanford.edu>
2014-01-30 21:59:08 -0800

    Basic functionality. Still have to make mapping correct

--- greatergiving_to_biddingforgood.py
+++ greatergiving_to_biddingforgood.py
@@ -15,9 +15,18 @@ parser.add_argument('outfile', nargs='?', type=argparse.FileType('wb'),
 
 args = parser.parse_args()
 
+b4g_header=['Lot Number', 'Quantity', 'Item Name', 'Item Description', 'Special Note', 'Donor Name', 'Donor URL', 'Donor Logo', 'Category', 'Item Type', 'Item Attributes', 'Item Location', 'Featured Item', 'Bidder Community', 'Cost', 'Estimated Value', 'Value Display', 'Online Bidding', 'Opening Bid', 'Reserve Price', 'Bid Increment', 'Buy Now Price', 'Online Open Date', 'Online Open Time', 'Online Close Date', 'Online Close Time', 'Status', 'Live Event', 'Item Image']
+
 def convert_gg_to_b4g(gg_dict):
     b4g_dict={}
+    b4g_dict['Lot Number'] = gg_dict['Item Number']
     b4g_dict['Item Name'] = gg_dict['Item Name']
+    b4g_dict['Item Description'] = gg_dict['Description']
+    b4g_dict['Donor Name'] = gg_dict['Donors']
+    b4g_dict['Item Location'] = gg_dict['Location']
+    b4g_dict['Cost'] = gg_dict['Cost']
+    b4g_dict['Estimated Value'] = gg_dict['Value']
+    b4g_dict['Category'] = gg_dict['Category Name']
     return b4g_dict
 
 gg_rows = []
@@ -25,9 +34,11 @@ input_file = csv.DictReader(args.infile)
 for row in input_file:
     gg_rows.append(row)
 
+sys.stderr.write(len(gg_rows) " rows read from input.")
+
 b4g_rows = map(convert_gg_to_b4g, gg_rows)
 
-output_file = csv.DictWriter(args.outfile, ['Item Name'], extrasaction='ignore', )
+output_file = csv.DictWriter(args.outfile, b4g_header, extrasaction='ignore')
 for row in b4g_rows:
     output_file.writerow(row)
     
